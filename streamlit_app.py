51b1d19ef6f3cd09b937c626ff778287f098fd05
Hyeonsoo7605 <comboy02@gmail.com>
2026-03-23 04:06:02 +0000

    데이터 시각화 기능 추가: Streamlit을 사용하여 데이터 입력 및 다양한 시각화 옵션 구현

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title('🎈 My new app: 데이터 시각화')
+st.write('왼쪽 사이드바에서 `data_visualization` 페이지를 선택하거나 아래 URL로 이동하세요.')
+st.write('- 기본 페이지: streamlit_app.py')
+st.write('- 시각화 페이지: pages/data_visualization.py')
+
+st.info('`Open App`에서 페이지가 뜨지 않으면 포트(8501) 확인 및 서버 재시작을 시도해보세요.')
